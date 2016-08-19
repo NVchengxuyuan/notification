d1c6d6cabf92fb339beeda3ff29983d882aed151
zhangcongcong <2094536013@qq.com>
2016-08-19 14:20:16 +0800

    Initial Commit

---
+++ Notification_DemoTests/Notification_DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  Notification_DemoTests.m
+//  Notification_DemoTests
+//
+//  Created by ZRY-Drex on 16/8/19.
+//  Copyright © 2016年 ZCC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Notification_DemoTests : XCTestCase
+
+@end
+
+@implementation Notification_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
