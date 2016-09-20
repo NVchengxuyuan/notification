6c59fefd8d9cc697bf0aaf3df8d0aedde35339b6
Nvchengxuyuan <2094536013@qq.com>
2016-09-20 16:26:47 +0800

    First commit

---
+++ Notification_Demo/NotificatonViewController.m
@@ -0,0 +1,37 @@
+//
+//  NotificatonViewController.m
+//  Notification_Demo
+//
+//  Created by ZRY-Drex on 16/8/19.
+//  Copyright © 2016年 ZCC. All rights reserved.
+//
+
+#import "NotificatonViewController.h"
+
+@interface NotificatonViewController ()
+
+@end
+
+@implementation NotificatonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
